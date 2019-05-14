2601584fa4c32661a2d63f98b66a6883e4c48191
AndreiCondurache98 <48328905+AndreiCondurache98@users.noreply.github.com>
2019-05-14 15:06:14 +0300

    Update Problem.java

--- Comparator-mark2/src/Problem.java
+++ Comparator-mark2/src/Problem.java
@@ -1,113 +1,13 @@
-import java.io.File;
-import java.io.IOException;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.*;
-import java.util.Dictionary;
-import java.util.Hashtable;
-import java.util.List;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.stream.StreamResult;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.xml.sax.SAXException;
 import java.util.*;
 
-
-
-/*
- * setup pt a merge:
- * am instalat mysql
- * am adaugat baza de date {coltul din dreapta sus unde scrie database) unde am adaugat nume/username/parola ale bazei de date create anterior si am pornit-o
- * am adaucat la File->Project structure->Module->Dependencies jar-ul (se gaseste in folderul din C unde este instalat mysql sau daca nu trebuie descarcat de pe net*/
-
 public class Problem {
+
     Connection myConn = Database.getConnection();
 
-    // returnez un document XML
-    public static Document createXML(ResultSet rs)
-            throws ParserConfigurationException, SQLException {
-        //se creeaza un nou document
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.newDocument();
-
-        //elementul ROOT
-        //radacina documentului XML
-        Element results = doc.createElement("Teste");
-        doc.appendChild(results);
-        //Obținem meta-datele
-        //acestea sunt folosite pentru a structura documentul
-        ResultSetMetaData rsmd = rs.getMetaData();
-        int colCount = rsmd.getColumnCount();
-
-        while (rs.next()) {   //adaugam elementele de tip Row
-            Element row = doc.createElement("Row");
-            results.appendChild(row);
-
-            for (int i = 1; i <= colCount; i++) {
-                String columnName = rsmd.getColumnName(i); //numele coloanei
-                Object value = rs.getObject(i); //vaoarea coloanei
-                //un nou element este creat pentru fiecare coloana si adaugat la randul corespunzator
-                Element node = doc.createElement(columnName);
-                node.appendChild(doc.createTextNode(value.toString()));
-                row.appendChild(node);
-            }
-        }
-        /*
-        // sciem continutul intr-un fisier XML
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        Transformer transformer = null;
-        try {
-            transformer = transformerFactory.newTransformer();
-            DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new File("C:\Users\Verde\Desktop"));
-            transformer.transform(source, result);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
-        return doc;
-    }
-
-    //o testare simpla sa vad daca merge
-    public void testIfFunctioning() {
-
-        try {
-            Statement myStmt = myConn.createStatement();
-            ResultSet myRs = myStmt.executeQuery("select * from enunturi_probleme");
-            while (myRs.next()) {
-                System.out.println(myRs.getString("titlu") + ": " + myRs.getString("enunt"));
-            }
-        } catch (Exception exc) {
-            exc.printStackTrace();
-        }
-    }
-
-    public String getEnuntProblema(int id) {
-        try {
-            //Statement myStmt = myConn.createStatement();
-            PreparedStatement statement = myConn.prepareStatement("SELECT * FROM enunturi_probleme" + " WHERE id = ?");
-            statement.setInt(1, id);
-            ResultSet rs = statement.executeQuery();
-            while (rs.next()) {
-                //System.out.println(rs.getString("enunt"));
-                return rs.getString("enunt");
-            }
-            return rs.getString("enunt");
-        } catch (Exception exc) {
-            exc.printStackTrace();
-        }
-        return null;
-    }
-
-    public Map<String, String> getTeste(int id) {
-
-        Document doc = null;
+    public Map<String, String> getTeste(int id)
+    {
         Map<String,String> dict = new HashMap<String,String>();
         try {
             //Statement myStmt = myConn.createStatement();
@@ -115,11 +15,6 @@ public class Problem {
                     "test4_in, test4_out from teste_probleme" + " WHERE id_problema = ?");
             statement.setInt(1, id);
             ResultSet rs = statement.executeQuery();
-
-
-
-//            doc = createXML(rs);
-
 
             while(rs.next())
             {
