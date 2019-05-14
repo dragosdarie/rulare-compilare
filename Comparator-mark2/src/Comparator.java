1e235dfb15acb1b1de44065ebf8d441db539c69e
AndreiCondurache98 <48328905+AndreiCondurache98@users.noreply.github.com>
2019-05-14 15:07:01 +0300

    Update Comparator.java

--- Comparator-mark2/src/Comparator.java
+++ Comparator-mark2/src/Comparator.java
@@ -2,60 +2,61 @@ import java.util.*;
 
 public class Comparator {
 
-//
-//    Problem pr = new Problem();
-//
-//
-//    Dictionary dict_bd = pr.getTeste(1);
-//    System.out.println(dict_bd);
-//
-
-    //INIT
     private int score;
+    private int nrTesteCorecte;
     private Map<String,String> database_tests;
     private Map<String,String> sandbox_tests;
 
     public Comparator() {
+        nrTesteCorecte = 0;
         score = 0;
         database_tests = new HashMap<String, String>();
         sandbox_tests = new HashMap<String, String>();
     }
 
-    //Get Test From DB
-    public Map<String, String> getTestFromDB(int id){
+    //Get Tests From DB
+    public Map<String, String> getTestsFromDB(int id){
         Problem pr = new Problem();
         return pr.getTeste(id);
     }
 
 
-    //Get Test From SandBox
+    //Get Tests From SandBox
     public Map<String, String> getTestsFromSB(int id){
         Problem pr = new Problem();
         return pr.getTeste(id);
     }
 
-
     //Compare Test and compute score
     public int compare_test(int id){
 
-        database_tests = getTestFromDB(id);
+        database_tests = getTestsFromDB(id);
         sandbox_tests = getTestsFromSB(id);
+        int nrTest = 1;
 
         for(String k: database_tests.keySet())
         {
             if(database_tests.get(k).equals(sandbox_tests.get(k))){
                 score += 25;
+                nrTesteCorecte ++;
+                System.out.println("Testul "+ nrTest + " este corect");
             }
+            else
+            {
+                System.out.println("Testul "+ nrTest + " este incorect");
+            }
+            nrTest ++;
         }
-
-        return score;
+        System.out.println("Punctaj: ");
+        return getScore();
     }
 
-
-    //return score to database
     public int getScore(){
         return score;
     }
 
-
+    public int getNrTesteCorecte() {
+        return nrTesteCorecte;
+    }
 }
+
